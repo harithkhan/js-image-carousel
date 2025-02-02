a59bad90f885ca39630ba4ce0421f074e6189780
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-02 08:55:36 +0800

    feat: add function slide left Add function slide left that when clicked, shows the image that is on the elft of the current image.

--- slide.js
+++ slide.js
@@ -2,12 +2,24 @@ export function slideRight() {
     const carousel = document.querySelector(".carousel");
     const regex = /(\d+)px/;
     const computedStyle = window.getComputedStyle(carousel);
-    const currentPosition = computedStyle.getPropertyValue("right")
+    const currentPosition = computedStyle.getPropertyValue("right");
     const match = currentPosition.match(regex);
-    console.log(match[1])
-    if (!carousel.style.right) {
-        carousel.style.right = "200px";
-    } else if (match[1] >= 200 && match[1] <= 600){
-        carousel.style.right = `${parseInt(match[1], 10) + 200}px`
+    console.log(`Before: ${match[1]}`);
+    if (match[1] >= 0 && match[1] <= 600) {
+        carousel.style.right = `${parseInt(match[1], 10) + 200}px`;
+        console.log(`After: ${computedStyle.getPropertyValue("right")}`);
     }
+}
+
+export function slideLeft() {
+    const carousel = document.querySelector(".carousel");
+    const regex = /(\d+)px/;
+    const computedStyle = window.getComputedStyle(carousel);
+    const currentPosition = computedStyle.getPropertyValue("left");
+    const match = currentPosition.match(regex);
+    console.log(`Before: ${match[1]}`);
+    if (match[1] > 0 && match[1] <= 800) {
+        carousel.style.right = `${parseInt(match[1], 10) - 200}px`;
+        console.log(`After: ${computedStyle.getPropertyValue("right")}`);
+    } 
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "./style.css";
-import { slideRight } from "../slide";
+import { slideRight, slideLeft } from "../slide";
 
 const rightButton = document.getElementById("right-button");
 rightButton.addEventListener("click", slideRight);
+
+const leftButton = document.getElementById("left-button");
+leftButton.addEventListener("click", slideLeft);
